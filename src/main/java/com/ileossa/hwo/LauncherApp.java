3935031d1c3c01c342db0d5c3f9d806e6b289c60
Vincent Lafosse <v.lafosse@oodrive.fr>
2016-07-05 16:27:42 +0200

    wip: init project

---
+++ src/main/java/com/ileossa/hwo/LauncherApp.java
@@ -0,0 +1,14 @@
+package com.ileossa.hwo;
+
+public class LauncherApp {
+
+	public LauncherApp() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+	}
+
+}
