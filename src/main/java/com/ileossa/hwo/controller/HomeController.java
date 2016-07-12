3aa8395a2681a1eddc9da56fa482715489207305
ileossa <you@example.com>
2016-07-12 12:58:36 +0200

    page d'acceuil, pour debug heroku

---
+++ src/main/java/com/ileossa/hwo/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.ileossa.hwo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by ileossa on 12/07/16.
+ */
+@RestController
+@RequestMapping()
+public class HomeController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String homePage(){
+        return "Welcom API Home Work Organizer";
+    }
+}
