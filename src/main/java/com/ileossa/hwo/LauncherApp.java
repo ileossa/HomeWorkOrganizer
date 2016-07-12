eba7d382becb3fa1f194c2f325ee3ab3e6cee8a5
ileossa <you@example.com>
2016-07-12 12:41:49 +0200

    nettoyage

--- src/main/java/com/ileossa/hwo/LauncherApp.java
+++ src/main/java/com/ileossa/hwo/LauncherApp.java
@@ -26,35 +26,4 @@ public class LauncherApp {
 	}
 
 
-	/*@Bean
-	public CommandLineRunner demo(UserRepository userRepository) {
-		return (args) -> {
-			userRepository.save(new UserModel("pseudo", "email", "password", "group", "role", true));
-            userRepository.save(new UserModel("pseudoqsqscqsc", "emaiqscsl", "paqsscqscsword", "gqcroup", "rqcqsole", false));
-
-			// fetch all customers
-			log.info("Customers found with findAll():");
-			log.info("-------------------------------");
-			for (UserModel hall : userRepository.findAll()) {
-				log.info( hall.toString());
-			}
-			log.info("");
-
-            // fetch an individual customer by ID
-            UserModel customer = userRepository.findOne(1L);
-            log.info("Customers found with findOne(1L):");
-            log.info("--------------------------------");
-            log.info(customer.toString());
-            log.info("");
-
-            // fetch customers by last name
-            log.info("Customers found with findByLastName('Bauer'):");
-            log.info("--------------------------------------------");
-            for (UserModel bauer : userRepository.findByPseudo("pseudo")) {
-                log.info(bauer.toString());
-            }
-            log.info("");
-		};
-	}*/
-
 }
