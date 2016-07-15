06b2435225810155e771b6c91f74bc3683b75a65
ileossa <you@example.com>
2016-07-15 12:42:59 +0200

    config H2 bdd, postgres fail connection

--- src/main/java/com/ileossa/hwo/configuration/DatabaseConfig.java
+++ src/main/java/com/ileossa/hwo/configuration/DatabaseConfig.java
@@ -6,12 +6,14 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 /**
  * Created by ileossa on 15/07/16.
  */
 @Configuration
+@Profile("prod")
 public class DatabaseConfig {
 
     @Bean
